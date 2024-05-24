d58febd680cf834dcb8c8a0371c08da9827d572c
Shashank mudgal <=>
2024-05-24 18:43:43 +0530

    Connected with database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 const PORT = 3001;
+
+mongoose.connect(process.env.MONGO).then((d)=>{
+    console.log("Database connected")
+})
 app.listen(PORT,()=>{
     console.log("your server is starting at "+ PORT)
 })
