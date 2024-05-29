107f631472f5b1c233eb65f0c61d5ecedf031d0c
Shashank mudgal <=>
2024-05-29 14:42:27 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 export const signup = async(req,res,next)=>{
     try {
         const {username,email,password} = req.body;
@@ -19,5 +20,24 @@ export const signup = async(req,res,next)=>{
         })
     } catch (error) {
        next(error)
+    }
+}
+export const signin = async(req,res,next)=>{
+    try {
+        const {email,password} = req.body;
+        const validUser = await User.findOne({email});
+       
+        if(!validUser){
+            return next(errorHandler(400,"User is not valid"));
+        }
+        const comp = bcryptjs.compareSync(password,validUser.password);
+        if(!comp){
+            return next(errorHandler(400,"Password is not valid"));
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:pass,...rest} = validUser._doc
+        return res.status(200).cookie('access_token',token,{httpsOnly:true}).json(rest)
+    } catch (error) {
+        next(error);
     }
 }
