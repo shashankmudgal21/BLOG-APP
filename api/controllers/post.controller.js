c194ffa7c91608e0a2a23ca1ba0467447b9493f0
Shashank mudgal <=>
2024-06-11 01:06:41 +0530

    completed image upload functionality fro create post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,7 +2,6 @@ import slugify from "slugify";
 import { errorHandler } from "../utils/error.js";
 import Post from "../models/post.model.js";
 export const createPost = async (req, res, next) => {
-  console.log(req.user);
   if (!req.user.isAdmin) {
     next(errorHandler(400, "You are not admin"));
   }
--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -1,9 +1,51 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react'
-import React from 'react'
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { Spinner } from 'flowbite-react';
+import  {getStorage, getDownloadURL, uploadBytesResumable, ref } from 'firebase/storage'
+import {app} from '../firebase'
 
 export default function CreatePost() {
+    const [files,setFiles] = useState(null);
+    const [imageProgress,setImageProgress] = useState(null)
+    const [imageError,setImageError] = useState(null)
+    const [formData,setFormData] = useState({});
+    console.log(formData)
+    const handleUploadImage = async()=>{
+        if(!files){
+            setImageError("select a image")
+            return};
+        try {
+            setImageError(null)
+            const storage = getStorage(app);
+            const fileName = new Date()+files
+            const storageRef = ref(storage,fileName);
+            const uploadTask = uploadBytesResumable(storageRef,files);
+            uploadTask.on(
+                'state_changed',
+                (snapshot)=>{
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setImageProgress(progress.toFixed(0));
+                },
+                (error) => {
+                    // Handle unsuccessful uploads
+                    setImageError("Image is not uploaded")
+                    setImageProgress(null)
+                }, 
+                () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                      setImageProgress(null)
+                      setFormData({...formData,image:downloadURL})
+                    });
+                }
+            )
+        } catch (error) {
+            setImageError(error.message)
+        }
+    }
   return (
     <div className='p-3 min-h-screen max-w-3xl mx-auto'>
         <h1 className='text-semibold text-3xl text-center my-4'>Create post</h1>
@@ -18,9 +60,13 @@ export default function CreatePost() {
                 </Select>
             </div>
             <div className='flex flex-row gap-4 justify-between items-center border-4 border-dotted p-3 border-teal-500'>
-                <FileInput type = "file" accept='images/*'/>
-                <Button gradientDuoTone={'purpleToPink'} outline>Upload Image</Button>
+                <FileInput type = "file" accept='images/*' onChange={(e)=>setFiles(e.target.files[0])}/>
+                <Button gradientDuoTone={'purpleToPink'} outline onClick={handleUploadImage}>
+                    {imageProgress ? <Spinner/> : "Upload"}
+                </Button>
             </div>
+            {imageError && <Alert color={'failure'}>{imageError}</Alert>}
+            {formData.image && <img className='w-full h-72 object-cover' src={formData.image}/>}
             <ReactQuill theme="snow" className='h-72 mb-10' required />
             <Button type='submit' gradientDuoTone={'purpleToPink'} outline> Publish</Button>
         </form>
