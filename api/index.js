30692574fcc0949d4b7fe7b0b066968971dcdc8d
Shashank mudgal <97382393+shashankmudgal21@users.noreply.github.com>
2024-07-18 00:21:36 +0530

    prepared it for deployment

--- api/index.js
+++ api/index.js
@@ -1,34 +1,41 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import postRouter from './routes/post.route.js'
-import commentRouter from './routes/comment.route.js'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 const app = express();
 const PORT = 3000;
+const __dirname = path.resolve();
+mongoose.connect(process.env.MONGO).then((d) => {
+  console.log("Database connected");
+});
 
-mongoose.connect(process.env.MONGO).then((d)=>{
-    console.log("Database connected")
-})
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/post',postRouter);
-app.use('/api/comment',commentRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal server error";
-    res.status(statusCode).json({
-        success:false,
-        message,
-    })
-    next();
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
 })
-app.listen(PORT,()=>{
-    console.log("your server is starting at "+ PORT)
-})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+  next();
+});
+app.listen(PORT, () => {
+  console.log("your server is starting at " + PORT);
+});
