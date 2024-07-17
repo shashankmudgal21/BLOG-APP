3e8bd84ddbb71ac62b4552d1f8c9567ca03ef4d5
Shashank mudgal <97382393+shashankmudgal21@users.noreply.github.com>
2024-07-18 00:13:08 +0530

    completed project

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      About
+      <div className='max-w-2xl mx-auto p-4 text-center min-h-screen flex flex-col justify-center'>
+        <div>
+          <h1 className='text-3xl text-center font-semibold my-7'>About shashank's blog</h1>
+          <div className='flex flex-col text-md text-gray-500 gap-6 p-3'>
+
+          <p>Welcome to Shashank's Blog! This blog was created by Shashank Mudgal as a personal project to share his thoughts and ideas with the world. Shashank is a passionate developer who loves to write about technology, coding, and everything in between.</p>
+          <p>Welcome to Shashank's Blog! This blog was created by Shashank Mudgal as a personal project to share his thoughts and ideas with the world. Shashank is a passionate developer who loves to write about technology, coding, and everything in between.</p>
+          <p>Welcome to Shashank's Blog! This blog was created by Shashank Mudgal as a personal project to share his thoughts and ideas with the world. Shashank is a passionate developer who loves to write about technology, coding, and everything in between.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import CallToAction from '../Components/CallToAction'
 export default function Projects() {
   return (
-    <div>
-      
+    <div className='max-w-2xl mx-auto p-3 text-center min-h-screen flex flex-col justify-center'>
+      <div className='flex flex-col gap-6 p-6'>
+
+      <h1 className='text-3xl text-center font-semibold my-2'>My project</h1>
+      <p className='text-md text-slate-500 my-2'>
+      Build fun and engaging projects while learning HTML, CSS, and JavaScript!
+      </p>
+      <CallToAction/>
+      </div>
     </div>
   )
 }
